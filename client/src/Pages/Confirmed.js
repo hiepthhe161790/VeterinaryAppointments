6c3f7ae6c7d715b04cb70898ee443f5d5e424fa2
hiepthhe161790 <hiepthhe161790@fpt.edu.vn>
2025-03-26 19:14:23 +0700

    a

--- client/src/Pages/Confirmed.js
+++ client/src/Pages/Confirmed.js
@@ -1,21 +1,23 @@
 import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 
-const Confirmed = (props) => {
+const Confirmed = () => {
   const history = useNavigate();
+  const { token } = useParams(); // Lấy tham số "token" từ URL
 
   useEffect(() => {
     (async () => {
       try {
-        await axios.post(`${process.env.REACT_APP_BACKEND_API_URL}/register`, { token: props.match.params.token });
+        await axios.post(`${process.env.REACT_APP_BACKEND_API_URL}/register`, { token });
         history("/");
       } catch (err) {
         console.log(err);
       }
     })();
-  }, [history, props.match.params.token]);
-  return <div>You are confirmed {props.match.params.token}</div>;
+  }, [history, token]);
+
+  return <div>You are confirmed {token}</div>;
 };
 
 export default Confirmed;
